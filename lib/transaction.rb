73a58e9c06fbcb6684481c2fd83b033b07445d35
samuel-c-johnson <samuel.c.johnson2@gmail.com>
2018-01-29 16:44:21 +0000

    Adds formatting to the transaction time

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -4,7 +4,7 @@ class Transaction
   attr_reader :date, :type, :amount, :balance
 
   def initialize(type, amount, balance)
-    @date = Date.today
+    @date = Date.today.strftime("%d/%m/%Y")
     @type = type
     @amount = amount
     @balance = balance

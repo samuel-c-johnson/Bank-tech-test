05de4952910d31fd0b1a3ec076878fe301741d1e
samuel-c-johnson <samuel.c.johnson2@gmail.com>
2018-01-29 12:28:53 +0000

    refactoring rspec tests to stay DRY

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,22 +2,25 @@ require 'account'
 
 describe Account do
 
+  let(:account) {Account.new}
+
   it 'is initialized with a balance of 0' do
-    account = Account.new
     expect(account.balance).to eq 0
   end
 
-  it 'allows user to deposit money' do
-    account = Account.new
-    account.deposit(1000)
-    expect(account.balance).to eq 1000
+  describe '#deposit' do
+
+    it 'allows user to deposit money' do
+      expect{ account.deposit(1000) }.to change{ account.balance }.from(0).to(1000)
+    end
   end
 
-  it 'allows user to withdraw money' do
-    account = Account.new
-    account.deposit(1000)
-    account.withdraw(250)
-    expect(account.balance).to eq 750
+  describe '#withdraw' do
+
+    it 'allows user to withdraw money' do
+      expect{ account.withdraw(250) }.to change{ account.balance }.by(-250)
+    end
+    
   end
 
 end
